17ab06e8237bbd2ee187af685f521c3cf569309f
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-09-30 18:12:17 -0300

    :rocket: Próximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import {
-  Container, Header, HeaderContent, Profile,
+  Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar,
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -31,6 +31,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src={user.avatar_url} alt="Fnoquiq" />
+
+              <strong>Gabriel Mesquita</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
